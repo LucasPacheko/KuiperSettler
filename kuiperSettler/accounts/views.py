e74b3ecf42664ba19bfe94b30b72e42b8889d041
Pacheco <06395706314@tjce.jus.br>
2024-02-14 20:18:15 -0300

    Firt commit

---
+++ kuiperSettler/accounts/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            return render(request, 'login.html', {'error_message': 'Invalid login'})
+    else:
+        return render(request, 'login.html')
+
+def logout(request):
+    logout(request)
+    return redirect('/')
