4a9d77183e43a1c02f62f44d110bf951ac51bffa
guilhermehhf <guilhermehenriquehf@gmail.com>
2024-03-12 22:41:14 -0300

    Setting up a frontend application in Next

---
+++ ks-frontend/app/user/login/page.tsx
@@ -0,0 +1,6 @@
+import FormLogin from "@/components/form-login";
+export default async function Page() {
+  return (
+    <FormLogin/>
+  );
+}
---
+++ ks-frontend/components/form-login.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { toast } from "@/components/ui/use-toast";
+const FormSchema = z.object({
+  email: z.string().email({ message: "Please enter a valid email address." }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters." }),
+});
+
+export default function FormLogin() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-white p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
+    console.log(data)
+  }
+
+  return (
+    <div className="flex justify-center items-center w-full h-screen">
+      <div className="bg-gradient-to-b from-slate-900 via-transparent to-transparent p-10 rounded-xl shadow-lg shadow-slate-700 w-full max-w-md">
+        <h1 className="text-center text-2xl font-bold mb-4 text-white">
+          KuiperSetter
+        </h1>
+        <div className="h-px bg-slate-500 opacity-50 my-6"></div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="email"
+                      autoComplete={"none"}
+                      placeholder="email@email.com"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+}
